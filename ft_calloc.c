5006c3a0d0d94473c3276e3f397ec8395153ce62
Pyae Phyo Zaw <pzaw@c2r2s7.42singapore.sg>
2024-05-30 19:46:54 +0800

    sch modified

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pzaw <pzaw@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 14:21:24 by pzaw              #+#    #+#             */
+/*   Updated: 2024/05/30 16:44:46 by pzaw             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void    *ft_calloc(size_t nmemb, size_t size)
@@ -11,14 +23,15 @@ void    *ft_calloc(size_t nmemb, size_t size)
     return (ptr);
 }
 
-/* int main(void)
+int main(void)
 {
     char *ptr;
     ptr = ft_calloc(6, sizeof(char));
     if (ptr == NULL)
     {
-        perror("ft_calloc failed");
+        printf("ft_calloc failed");
         return (1);
     }
+    printf("%s \n", ptr);
     return (0);
-} */
+}
